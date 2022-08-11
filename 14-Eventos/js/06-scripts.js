5dda4b7e25345b12bed9d8396cac5ac4bea583f9
Norman Arredondo <arredondo_norman25@outlook.es>
2022-08-10 23:44:17 -0500

    Event Bubbling

---
+++ 14-Eventos/js/06-scripts.js
@@ -0,0 +1,22 @@
+/**Event Bubbling */
+
+const cardDiv = document.querySelector('.card')
+const infoDiv = document.querySelector('.info')
+const titulo = document.querySelector('.titulo')
+
+cardDiv.addEventListener('click', (e) => {
+    e.stopPropagation()
+    console.log('click en card') 
+})
+
+infoDiv.addEventListener('click', (e) => {
+    e.stopPropagation()
+    console.log('click en info') 
+})
+
+titulo.addEventListener('click', (e) => {
+    e.stopPropagation()
+    console.log('click en titulo') 
+})
+
+//Evitamos que se propabe agregando el evento y stopPropagation
